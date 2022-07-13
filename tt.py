90d13e3e178931fde50967295b02adf0ccf79266
Jang GeonWu <today4656fl@naver.com>
2022-07-13 17:58:23 +0900

    0713_update_3

--- tt.py
+++ tt.py
@@ -1,21 +1,12 @@
-def solution(n):
-    n = n - 1
-    answer = ''
-    while True:
-        
-        num1 = n % 3
-        num2 = n // 3
-        if num1 == 2:
-            answer = '4' + answer
-        else:
-            answer = str(num1 + 1) + answer
-        
-        if num2 > 0:
-            n = num2 - 1
-        else:
-            break
+from math import ceil
+def solution(w,h):
+    ratio = w / h if w > h else h / w
     
-    return answer
+    num_square = ceil(ratio)
+    
+    loss = h * num_square if w > h else w * num_square
+    
+    
+    return (w*h) - loss
 
-for i in range(1, 20):
-    print(solution(i))
+print(solution(3,3))    
